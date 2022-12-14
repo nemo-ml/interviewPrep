7b12678cfc19abfe099ebcd3e01a2c0d8eb759b2
nemo-ml <mohd.naimuddin94@gmail.com>
2022-12-15 00:13:18 +0530

    Adding Theory

--- interviewPrep/VirtualFunc_implement.cpp
+++ interviewPrep/VirtualFunc_implement.cpp
@@ -28,3 +28,13 @@ class Manager : public Employee
 		//
 	}
 };
+
+/*
+Limitations of Virtual Functions :
+
+Slower: The function call takes slightly longer due to the virtual mechanismand makes 
+it more difficult for the compiler to optimize because it does not know exactly which 
+function is going to be called at compile time. Difficult to Debug : In a complex system,
+virtual functions can make it a little more difficult to figure out where a function is
+being called from.
+*/
