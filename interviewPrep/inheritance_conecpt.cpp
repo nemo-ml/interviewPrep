e8a7627c0e04001f1aa4fecac372904e14b094d8
nemo-ml <mohd.naimuddin94@gmail.com>
2022-12-16 01:05:36 +0530

    C++ program to demonstrate implementation of Inheritance

--- interviewPrep/inheritance_conecpt.cpp
+++ interviewPrep/inheritance_conecpt.cpp
@@ -22,5 +22,86 @@ subclass is called Base Class or Superclass.
 
 */
 
+// Example: define member function without argument within the class
+
 #include <iostream>
 
+class Person
+{
+	int id;
+	char name[100];
+public:
+	void set_p()
+	{
+		std::cout << "Enter the ID:" << std::endl;
+		std::cin >> id;
+		std::cout << "Enter the Name:" << std::endl;
+		std::cin.get(name, 100);
+	}
+
+	void Display_p()
+	{
+		std::cout << name << "\t" << id << "\t" << std::endl;
+	}
+};
+
+class Student : private Person
+{
+	char course[50];
+	int fee;
+public:
+	void set_s()
+	{
+		set_p();
+		std::cout << "Enter the Course Name:" << std::endl;
+		std::cin.getline(course, 50);
+		std::cout << "Enter the Course Fee:" << std::endl;
+		std::cin >> fee;
+	}
+
+	void Display_s()
+	{
+		Display_p();
+		std::cout << course << "\t" << fee << std::endl;
+	}
+};
+
+int main()
+{
+	Student studentObject;
+
+	studentObject.set_s();
+	studentObject.Display_s();
+
+	return 0;
+}
+
+
+// C++ program to demonstrate implementation of Inheritance
+#include<bits/stdc++.h>
+
+
+class Parents						// Base Class 
+{
+public:
+	int id_p;
+};
+
+class Child : public Parents		// Sub class inheriting from Base Class(Parent)
+{
+public:
+	int id_c;
+};
+
+int main()							// main function
+{
+	Child childObject;				// An object of class child has all data members
+									// and member functions of class parent
+	childObject.id_c = 7;
+	childObject.id_p = 10;
+
+	std::cout << "Child id is: " << childObject.id_c << '\n' << std::endl;
+	std::cout << "Parents id is: " << childObject.id_p << '\n' << std::endl;
+
+	return 0;
+}
