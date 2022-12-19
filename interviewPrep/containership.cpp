1a8681a143c755b379957adfd2ead0964ae558a4
nemo-ml <mohd.naimuddin94@gmail.com>
2022-12-20 02:22:52 +0530

    Concept of Containership

---
+++ interviewPrep/containership.cpp
@@ -0,0 +1,12 @@
+/*
+* 
+* Containership:
+When an object of one class is created into another class then that object will be a member of 
+that class, this type of relationship between the classes is known as containership or has_a 
+relationship as one class contains the object of another class.
+
+The class which contains the object and members of another class in this kind of relationship is 
+called a container class and the object that is part of another object is called a contained object
+whereas the object that contains another object as its part or attribute is called a container object.
+*
+* /
