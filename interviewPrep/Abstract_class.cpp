8c35d6fc4d035e85e31fe89054e74a0d84f133b6
nemo-ml <mohd.naimuddin94@gmail.com>
2022-12-26 22:20:55 +0530

    updated

--- interviewPrep/Abstract_class.cpp
+++ interviewPrep/Abstract_class.cpp
@@ -7,6 +7,7 @@ have implementation, we only declare it.
 A pure virtual function is declared by assigning 0 in declaration.See the following example.
 
 // An abstract class
+
 class Test {
     // Data members of class
 public:
