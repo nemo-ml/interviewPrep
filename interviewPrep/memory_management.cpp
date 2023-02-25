c509387bba8cf98c93fabe9e2b678fe732d6897d
nemo-ml <mohd.naimuddin94@gmail.com>
2023-02-25 23:38:50 +0530

    memory_management

---
+++ interviewPrep/memory_management.cpp
@@ -0,0 +1,11 @@
+/*A typical memory representation of a C program consists of the following sections.
+
+Text segment(i.e.instructions)
+Initialized data segment
+Uninitialized data segment(bss)
+Heap
+Stack
+
+
+*/
+
